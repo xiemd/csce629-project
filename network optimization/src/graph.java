c5739b225661855f4dc3813fdd028b78feb2fc71
Mindong Xie <Mindong Xie>
2017-03-25 16:27:02 -0500

    finish graph generator

--- network optimization/src/graph.java
+++ network optimization/src/graph.java
@@ -7,33 +7,43 @@ public class graph {
 	
 	public graph(int vertices){
 		this.vertices = vertices;
-		adj = new ArrayList[vertices];
-		for (int i = 0; i < vertices; i++ ){
+		this.edge = 0;
+		adj = new ArrayList[vertices + 1];
+		for (int i = 1; i <= vertices; i++ ){
 			adj[i] = new ArrayList<Edge>();
 		}
 	}
-	public void addEdge(Edge e){
-		validate(e.start, vertices);
-		validate(e.end, vertices);
-		this.adj[e.start].add(e);
-		this.adj[e.end].add(e);
-		edge++;
+	public void addEdge(int start, int end, int weight){
+		Edge e = new Edge(start, end);
+		e.weight = weight;
+		if (!this.adj[start].contains(e)){
+			this.adj[start].add(e);
+			this.adj[end].add(e);
+			edge++;
+		}
 	}
 	
 	public int getWeight(int start, int end){
 		int result = 0;
 		ArrayList<Edge> list = adj[start];
 		for (Edge e : list){
-			if (e.end == end){
-				result = e.weight;
-				break;
+//			if the edge's end is equal to the parameter start, then compare the start with parameter end
+			if (e.end == start){
+				if (e.start == end){
+					result = e.weight;
+					break;
+				}
+			}else
+//			means the edge's start is the parameter start;
+			{
+				if (e.end == end){
+					result = e.weight;
+					break;
+				}
 			}
+
 		}
 		return result;
 	}
-	
-	private void validate(int start, int vertices){
-		if (start < 0 || start >= vertices) throw new IllegalArgumentException("the starting node is not in the right range");
-				
-	}
 }
+	
--- network optimization/src/maxHeap.java
+++ network optimization/src/maxHeap.java
@@ -1,16 +1,34 @@
 import java.lang.Math;
+import java.util.Arrays;
 
 public class maxHeap {
-	private int[] vertices; //record the vertices
-	private int[] values; // record the vertices' value
+	
+
+	int[] vertices; //record the vertices name of the heap
+	int[] values; // record the vertices'value
+	int[] pos;//record the vertices' position
 	int size = 0;
+	
+//	constructor for testing
+	public maxHeap(int number) {
+		vertices = new int[number + 1];
+		values = new int[number + 1];
+		pos = new int[number + 1];
+		for (int i = 1; i <= number; i++){ 
+			vertices[i] = 0; // i is the position of the heap, value is the vertex's name
+			values[i] = 0; // i is the vertex's name, return the value of the vertex
+			pos[i] = 0;//i is the vertex's name, return the position on the heap
+		}
+	}
 	
 	public maxHeap(graph g) {
 		vertices = new int[g.vertices + 1];
 		values = new int[g.vertices + 1];
+		pos = new int[g.vertices + 1];
 		for (int i = 1; i <=g.vertices; i++){ 
-			vertices[i] = 0; 
-			values[i] = i;
+			vertices[i] = 0; // i is the position of the heap, value is the vertex's name
+			values[i] = 0; // i is the vertex's name, return the value of the vertex
+			pos[i] = 0;//i is the vertex's name, return the position on the heap
 		}
 	}
 //	return the vertex with maximum value
@@ -18,63 +36,77 @@ public class maxHeap {
 		return vertices[1];
 	}
 	
-	public void insert(int vertex){
-		vertices[this.size + 1] = vertex;
+	public void insert(int vertex, int value){
 		size++;
+		vertices[size] = vertex;
+		pos[vertex] = size; 
+		values[vertex] = value;
 		heapify(size);
-		
 	}
 	public void delete(int vertex){
-		int position = 0;
+		int position = pos[vertex];
 //		find the vertex's position, swap it with the last vertex, and delete the last vertex;
-		for (int i = 1; i < size + 1; i++){
-			if (vertices[i] == vertex){
-				position = i;
-				swap(position, size);
-				vertices[size] = 0;
-				size--;
-				break;
-			}
-		}
-//		heapify the vertex in this position
+		swap(position, size);
+		vertices[size] = 0;
+		pos[vertex] = 0;
+		size--;
 		heapify(position);
 	}
 	
 	private void heapify(int position){
-//		move the vertex up or down to the right position
-		int currentVal = values[vertices[position]];
+		
+		int father = position / 2;
+		int lson = 2 * position;
+		int rson = 2 * position + 1;
+		int vertex = vertices[position];
 //		go up if position is not 1 and value is larger than father
-		if (position > 1 && currentVal >= values[vertices[position / 2]]){
-			int fatherVal = values[vertices[position / 2]];
-			int h = position;
-			while (h > 1 && currentVal >= fatherVal){
-				swap(h, h / 2);
-				h = h / 2;
-			}
-//		go down if position is not leaf and value is smaller than maximum child
-		}else if(position * 2 <= size && values[vertices[position]] < Math.max(values[vertices[position * 2]], values[vertices[position * 2 + 1]])){
-			int h = position;
-			int sonVal1 = values[vertices[position * 2]];
-			int sonVal2 = values[vertices[position * 2 + 1]];
-			
-			while (h * 2 <= size && currentVal < Math.max(sonVal2, sonVal1)){
-				if (sonVal1 >= sonVal2){
-					swap(h, h * 2);
-					h = h * 2;
-				}else{
-					swap(h, h * 2 + 1);
-					h = h * 2 + 1;
-				}
-			}
-		}else{ 
+		
+		if (position > 1 && values[vertex] >= values[vertices[father]]){
+			swap(position, father);
+			heapify(father);
+		}
+//		if the position has one child and smaller than it then swap with it 
+		else if (lson == size && values[vertex] < values[vertices[lson]]){
+			swap(position, lson);
 			return;
 		}
+//		if the position has two child and smaller than its max child, swap with the larger one
+		else if (rson <= size && values[vertex] < Math.max(values[vertices[lson]], values[vertices[rson]])){
+			if(values[vertices[lson]] >= values[vertices[rson]]){
+				swap(position, lson );
+				heapify(lson);
+			}else{
+				swap(position, rson);
+				heapify(rson);
+			}
+		}
+		else return;
 	}
 	
+	public String getValues(){
+		return  Arrays.toString(values);
+	}
+	
+	public String getVertices(){
+		return  Arrays.toString(vertices);
+	}
+
+
 	private void swap (int position1,int position2){
-		int temp = vertices[position1];
-		vertices[position1] = vertices[position2];
+		int vertex1 = vertices[position1];
+		int vertex2 = vertices[position2];
+		
+//		update the vertex position in verteices[]
+		int temp = vertex1;
+		vertices[position1] = vertex2;
 		vertices[position2] = temp;
+		
+//		update the pos[] index of those vertices
+		int tempPos = pos[vertex1];
+		pos[vertex1] = pos[vertex2];
+		pos[vertex2] = tempPos;
+		
 	}
 }
+
 	
